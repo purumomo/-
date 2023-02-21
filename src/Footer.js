d81ff53ff52ed9074b8b16f84df9ba7f1b30f9b4
purumomo <fengning65@gmail.com>
2023-02-21 12:09:03 +0900

    update 20230221

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p>
-        &copy;  2012-2023 Copyright by Purumomo Ltd.
+        &copy;  2012-2023 Copyright by Purumomo.
       </p>
     </footer>
   );
